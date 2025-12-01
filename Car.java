23c59c7e12b254b4958a59a478384b65db097056
MoonLigth247 <firesfires330@gmail.com>
2025-12-02 02:15:23 +0800

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,19 +1,19 @@
 package biydaalt2;
 
 public class Car {
-	private String chiglel;
-	private String dugaar;
-	
-	public Car(String ch, String d){
-		chiglel = ch;
-		dugaar = d;
-	}
-	
-	String getChiglel() {
-		return chiglel;
-	}
-	
-	String getDugaar() {
-		return dugaar;
-	}
+    private String chiglel;
+    private String dugaar;
+    
+    public Car(String ch, String d){
+        chiglel = ch;
+        dugaar = d;
+    }
+    
+    public String getChiglel() {
+        return chiglel;
+    }
+    
+    public String getDugaar() {
+        return dugaar;
+    }
 }
